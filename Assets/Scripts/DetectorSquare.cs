2892c06569711cd838fccb15c0fe5750559b6ab4
GARGHALON\jonah <jonahaklee@gmail.com>
2024-10-07 20:04:36 -1000

    Made the floor unsmooth and added collision to all levels.

--- Assets/Scripts/DetectorSquare.cs
+++ Assets/Scripts/DetectorSquare.cs
@@ -5,14 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class DetectorSquare : MonoBehaviour
 {
- 
+
+
     void OnTriggerEnter2D
         (Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Debug.Log ("The Player has been detected");
-            SceneManager.LoadScene("Level 1");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
